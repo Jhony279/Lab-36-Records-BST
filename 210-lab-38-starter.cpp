924fed4e00dd2bf5e811e556c4b50f965a39347b
Jhony279 <lordjhony732@gmail.com>
2026-05-12 00:05:52 -0700

    Added last 2 functions to the menu

--- 210-lab-38-starter.cpp
+++ 210-lab-38-starter.cpp
@@ -41,7 +41,22 @@ int main() {
                 break;
 
             case 4:
+                cout << "Enter code to modify --> ";
+                cin >> code;
+                if (tree.searchNode(code)) {
+                    cout << "Found " << code << " in the tree.\n";
+                    cout << "Enter new code --> ";
+                    string new_code;
+                    cin >> new_code;
+                    tree.remove(code);
+                    tree.insertNode(new_code);
+                } else
+                    cout << code << " is not in the tree.\n";
                 break;
+
+            case 5:
+                again = false;
+            break;
 
             default:
                 cout << "\nInvalid selection.\n";
