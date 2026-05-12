39a8a43ff465748c12e3e36222a5e9293fa2392d
Jhony279 <lordjhony732@gmail.com>
2026-05-11 23:18:48 -0700

    Creating driver code for binary tree library by inserting node and displaying

--- 210-lab-38-starter.cpp
+++ 210-lab-38-starter.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 #include "IntBinaryTree.h"
+#include <string>
 using namespace std;
 
 int main() {
-    
+    IntBinaryTree tree;
+    tree.insertNode("D");
+    tree.insertNode("B");
+
+    tree.displayInOrder();
+    tree.displayPreOrder();
+    tree.displayPostOrder();
 
     return 0;
 }
