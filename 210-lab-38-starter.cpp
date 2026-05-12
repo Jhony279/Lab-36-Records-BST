e9e9ce394b8df9c293325b36e6f4e8daa53d96f9
Jhony279 <lordjhony732@gmail.com>
2026-05-11 23:28:18 -0700

    Creating a method to let user use the menu

--- 210-lab-38-starter.cpp
+++ 210-lab-38-starter.cpp
@@ -3,14 +3,35 @@
 #include <string>
 using namespace std;
 
+const int MAX_OPTIONS = 4;
+
+int main_menu();
+
 int main() {
     IntBinaryTree tree;
-    tree.insertNode("D");
-    tree.insertNode("B");
+    bool again = true;
+    string code;
 
     tree.displayInOrder();
     tree.displayPreOrder();
     tree.displayPostOrder();
 
     return 0;
+}
+
+int main_menu() {
+    cout << "\n*** GOAT MANAGER 3001 ***\n";
+    cout << "[1] Add Node\n";
+    cout << "[2] Delete Node\n";
+    cout << "[2] Search Node\n";
+    cout << "[4] Modify Node\n";
+    cout << "Choice --> ";
+
+    int choice;
+    cin >> choice;
+    while (choice < 1 || choice > MAX_OPTIONS) {
+        cout << "Invalid, again --> ";
+        cin >> choice;
+    }
+    return choice;
 }
